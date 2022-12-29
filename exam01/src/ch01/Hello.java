a343c0094d59ed677302e6a980c6e79215959ea6
doccomsa <30486289+doccomsa@users.noreply.github.com>
2022-12-29 10:57:53 +0900

    Update Hello.java

--- exam01/src/ch01/Hello.java
+++ exam01/src/ch01/Hello.java
@@ -5,7 +5,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World1");
 		System.out.println("Hello World2");
-		System.out.println("Hello World3");
 
 	}
 
